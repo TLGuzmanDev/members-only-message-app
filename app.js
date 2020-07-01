85efa46c0ed5bfc20a364b1617d9c2e57c8eaffa
Tony G <tlguzman94@gmail.com>
2020-07-01 19:55:00 -0400

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const indexRouter = require('./routes');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}`);
+});
